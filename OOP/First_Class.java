37530bb6375f814b69398d523bec56392294ee19
Sachin Chakrawarti <104677949+ChakrawartiSachin1@users.noreply.github.com>
2024-04-19 17:15:38 +0000

    .

---
+++ OOP/First_Class.java
@@ -0,0 +1,16 @@
+class Love {
+    String First_Love = "Sharddhu ji";
+    String Second_Love = "Priyal ji";
+}
+
+public class First_Class {
+
+    public static void main(String[] args) {
+
+        Love Pyar = new Love();
+        System.out.println("First Love : " + Pyar.First_Love);
+        System.out.println("Second Love : " + Pyar.Second_Love);
+
+    }
+
+}
