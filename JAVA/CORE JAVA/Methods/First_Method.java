7b796dafe37e5c1fd9da2d30b1cde36a0f70f45d
Sachin Chakrawarti <104677949+ChakrawartiSachin1@users.noreply.github.com>
2024-04-19 12:15:33 +0000

    .

---
+++ JAVA/CORE JAVA/Methods/First_Method.java
@@ -0,0 +1,17 @@
+public class First_Method {
+    
+        public static String love(String first, String second) {
+    
+            String My_loves = first + " & " + second;
+    
+            return My_loves;
+    
+        }
+    
+        public static void main(String[] args) {
+            String Heart = love("Shraddhu ji ", "Priyal ji");
+            System.out.println("Both are My ture Love " + Heart);
+        }
+    
+    }
+    
