8b6cfb7b0fed27e985695d018d8ba7bdb3a40aa4
Sachin Chakrawarti <104677949+ChakrawartiSachin1@users.noreply.github.com>
2024-04-23 16:53:48 +0000

    .

--- OOP/Classes and Objects/First_Class_Obj.java
+++ OOP/Classes and Objects/First_Class_Obj.java
@@ -1,16 +1,14 @@
 public class First_Class_Obj {
 
-    public class Car {
-        String Brand = Tata;
-        String Color = red;
-
+    public static class Car {
+        String Brand = "Tata"; // Enclose string literals in double quotes
+        String Color = "red";  // Enclose string literals in double quotes
     }
 
     public static void main(String[] args) {
         Car mycar = new Car();
 
-        System.out.println("Brand " + mycar.Brand);
-        System.out.println("Color " + mycar.Color);
+        System.out.println("Brand: " + mycar.Brand);
+        System.out.println("Color: " + mycar.Color);
     }
-
 }
